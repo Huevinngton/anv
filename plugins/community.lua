6812c5a5c891b0a94d9fb9d18a0fb11ce6e43b98
Huevinngton <agusmadafaka@gmail.com>
2023-04-19 15:51:44 -0300

    ashe

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,8 +3,13 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  { import = "astrocommunity.colorscheme.nightfox" },
   { import = "astrocommunity.color.twilight-nvim" },
   { import = "astrocommunity.color.modes-nvim" },
   { import = "astrocommunity.color.tint-nvim" },
   { import = "astrocommunity.media.presence-nvim" },
+  { import = "astrocommunity.terminal-integration.vim-tmux-yank" },
+  { import = "astrocommunity.terminal-integration.vim-tpipeline" },
+  { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
+  { import = "astrocommunity.editing-support.dial-nvim" },
 }
